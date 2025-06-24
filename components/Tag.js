1630c07d2b1c5c49b4b5125aad687971f90bf3d2
Subhasish Mishra <msubhasish11@gmail.com>
2025-06-25 00:00:41 +0530

    dynamic tag color issue fixed

--- components/Tag.js
+++ components/Tag.js
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
 
-const Tag = ({ data, removeTag, tagColor }) => {
+const Tag = ({ data, removeTag, color = 'bg-blue-200 text-blue-700' }) => {
   return (
     <div
-      className={`inline-flex items-center bg-${tagColor}-200 text-${tagColor}-700 px-2 py-1 rounded-lg mr-1`}
+      className={`inline-flex items-center ${color} px-2 py-1 rounded-lg mr-1`}
     >
       <span>{data.name}</span>
       <button
